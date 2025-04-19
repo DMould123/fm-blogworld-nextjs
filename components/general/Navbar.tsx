a5077c8dc650cd2bc708a917071cb1eeac9d3600
DMould123 <david.mould123@yahoo.com>
2025-04-19 20:48:46 +0200

    Started creating a Navbar component. Installded shadcn

---
+++ components/general/Navbar.tsx
@@ -0,0 +1,34 @@
+import Link from "next/link";
+import { Button } from "../ui/button";
+
+export function Navbar() {
+  return (
+    <nav className="py-5 flex items-center justify-between">
+      <div className="flex items-center gap-6">
+        <Link href="/">
+          <h1 className="text-red-500 text-3xl font-semibold">
+            FM<span className="text-blue-500">BlogWorld</span>
+          </h1>
+        </Link>
+        <div className="hidden sm:flex items-center gap-6">
+          <Link
+            className="text-sm font-medium hover:text-blue-500 transition-colors"
+            href="/"
+          >
+            Home
+          </Link>
+          <Link
+            className="text-sm font-medium hover:text-blue-500 transition-colors"
+            href="/dashboard"
+          >
+            Dashboard
+          </Link>
+        </div>
+      </div>
+      <div className="flex items-center gap-4">
+<Button>Login</Button>
+<Button>Register</Button>
+      </div>
+    </nav>
+  );
+}
