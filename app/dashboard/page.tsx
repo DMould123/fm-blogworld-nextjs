203664ea3bb09506e1ff78bb0a7324a9e6ee165e
DMould123 <david.mould123@yahoo.com>
2025-04-23 00:11:36 +0200

    Added middleware to manage authentication and protect routes.- Configured `publicPaths` for unauthenticated access. Updated Navbar to fetch and display user session data using `getKindeServerSession`.

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,8 +1,12 @@
-export default function DashboardRoute() {
+
+
+export default async function DashboardRoute() {
+
+
   return (
     <div className="py-6">
       <h1 className="text-3xl font-bold tracking-tight mb-8">Dashboard</h1>
-      <p>Dashboard content goes here.</p>
+      <p>Welcome</p>
     </div>
-  );
+  )
 }
--- components/general/Navbar.tsx
+++ components/general/Navbar.tsx
@@ -1,8 +1,12 @@
 import Link from "next/link";
-import { Button, buttonVariants } from "../ui/button";
-import {RegisterLink, LoginLink} from "@kinde-oss/kinde-auth-nextjs/components";
+import { buttonVariants } from "../ui/button";
+import { RegisterLink, LoginLink, LogoutLink } from "@kinde-oss/kinde-auth-nextjs/components";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 
-export function Navbar() {
+export async function Navbar() {
+  const {getUser} = getKindeServerSession();
+  const user = await getUser();
+
   return (
     <nav className="py-5 flex items-center justify-between">
       <div className="flex items-center gap-6">
@@ -26,10 +30,23 @@ export function Navbar() {
           </Link>
         </div>
       </div>
-      <div className="flex items-center gap-4">
-      <LoginLink className={buttonVariants()}>Sign in</LoginLink>
-      <RegisterLink className={buttonVariants({variant: "secondary"})}>Register</RegisterLink>
+
+      <div>
+        {user ? (
+          <div className="flex items-center gap-4">
+            <p>{user.given_name}</p>
+            <LogoutLink className={buttonVariants({ variant: "secondary" })}>Log Out</LogoutLink>
+          </div>
+        ) : (
+          <div className="flex items-center gap-4">
+            <LoginLink className={buttonVariants()}>Sign in</LoginLink>
+            <RegisterLink className={buttonVariants({ variant: "secondary" })}>
+              Register
+            </RegisterLink>
+          </div>
+        )}
       </div>
     </nav>
   );
 }
+
